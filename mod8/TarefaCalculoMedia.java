137b0f56cd7e2cfda12a49750b747fad326b70e3
Kaio Muller <118767668+Kaiomuller@users.noreply.github.com>
2023-09-14 12:32:40 -0300

    TarefaCalculoMedia.java

--- mod8/TarefaCalculoMedia.java
+++ mod8/TarefaCalculoMedia.java
@@ -1,1 +1,20 @@
+public class NotaAluno {
+
+    private double nota1 = 9.4;
+    private double nota2 = 9;
+    private double nota3 = 6.7;
+    private double nota4 = 6.8;
+
+    public double mediaNotas(){
+        double somaNotas = (nota1 + nota2 + nota3 + nota4) / 4;
+        double media = somaNotas;
+        return media;
+    }
+
+    public void resultadoMedia(){
+        System.out.println();
+        System.out.printf("  A média do aluno é %.2f %n", + mediaNotas());
+    }
+
+}
 
